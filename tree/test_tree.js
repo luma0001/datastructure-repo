cb4d39cdacc47088466f42c94049bbe4a6fd35f5
SirShagalot <124779092+luma0001@users.noreply.github.com>
2024-12-01 17:54:33 +0100

    completed addValue

--- tree/test_tree.js
+++ tree/test_tree.js
@@ -3,28 +3,51 @@ window.addEventListener("load", start);
 
 import { Node, Tree } from "./tree.js";
 
+// ======================== Test the nodes functions
+
 function start() {
   console.log("Start");
-  const nodeA = new Node(null, [], 5);
-  const nodeB = new Node(nodeA, [], 50);
+  const nodeA = new Node(null, [], "A");
+  const nodeB = new Node(nodeA, [], "B");
   nodeA.appendChild(nodeB);
-  const nodeC = new Node(nodeA, [], 75);
+  const nodeC = new Node(nodeA, [], "C");
   nodeA.appendChild(nodeC);
+  const nodeD = new Node(nodeA, [], "D");
+  nodeA.appendChild(nodeD);
+  const nodeE = new Node(nodeB, [], "E");
+  nodeB.appendChild(nodeE);
+  const nodeF = new Node(nodeD, [], "F");
+  nodeD.appendChild(nodeF);
+  const nodeG = new Node(nodeE, [], "G");
+  nodeE.appendChild(nodeG);
 
   console.log(nodeB);
   console.log(nodeC);
   console.log(nodeA);
 
-  console.log("Does B have children?");
-  console.log(nodeB.hasChildNodes());
-  console.log("Does A have chidren?");
-  console.log(nodeA.hasChildNodes());
-  console.log("A's fisrt child");
-  console.log(nodeA.firstChid());
-  console.log("A's last child");
-  console.log(nodeA.lastChild());
-  console.log("Remove B from A");
-  const b = nodeA.returnChildAtIndex(1);
-  console.log(nodeA.removeChild(b));
-  console.log(nodeA);
+  // console.log("Does B have children?");
+  // console.log(nodeB.hasChildNodes());
+  // console.log("Does A have chidren?");
+  // console.log(nodeA.hasChildNodes());
+  // console.log("A's fisrt child");
+  // console.log(nodeA.firstChid());
+  // console.log("A's last child");
+  // console.log(nodeA.lastChild());
+  // console.log("Remove C from A");
+  // // const c = nodeA.returnChildAtIndex(1);
+  // console.log(nodeA.removeChild(nodeC));
+  // console.log(nodeA);
+  // console.log("Replace B with D");
+  // console.log(nodeA.replaceChild(nodeD, nodeB));
+  // console.log(nodeA);
+
+  // ======================== Test the tree functions
+  const tree = new Tree(nodeA);
+  console.log(tree);
+
+  console.log("Dump the tree");
+  tree.dump();
+
+  console.log("Find value");
+  console.log(tree.findValue("E"));
 }
--- tree/tree.js
+++ tree/tree.js
@@ -1,3 +1,4 @@
+
 class Node {
   constructor(parent, children, value) {
     this.parent = parent;
@@ -44,19 +45,103 @@ class Node {
     return childToRemove;
   }
 
-  // replaceChild( newChild, oldChild ){
-
-  // }
+  replaceChild(newChild, oldChild) {
+    let childToReplace = null;
+    for (let i = 0; i < this.childNodes.length; i++) {
+      console.log(i);
+      if (this.childNodes[i] === oldChild) {
+        console.log("Replace " + oldChild);
+        childToReplace = this.childNodes[i];
+        this.childNodes[i] = newChild;
+      }
+    }
+    return childToReplace;
+  }
 }
 
 class Tree {
   constructor(node) {
-    root: node;
+    this.root = node;
   }
 
   root() {
     return this.root;
   }
+
+  dump() {
+    const treeRoot = this.root;
+    console.log(treeRoot);
+
+    let generation = 0;
+    const generationList = [];
+    for (const child of treeRoot.childNodes) {
+      generationList.push(child);
+    }
+
+    logChildrenOfNode(this.root);
+
+    function logChildrenOfNode(node) {
+      // let newGenerationList = [];
+      // if (generationList.find(node)) {
+      //   generation = generation;
+      //   for (const child of node) {
+      //     newGenerationList.push(child);
+      //     generationList.push(newGenerationList);
+      //   }
+      // }
+      generation++;
+
+      console.log("--------------------------------------");
+      if (node != treeRoot) {
+        console.log("Parrent node: ", node.parent.value);
+      }
+      // console.log("Generation: ", generation);
+      console.log("All children in node: ", node);
+      for (const child of node.childNodes) {
+        console.log("-----------child-----------");
+        console.log(child);
+        console.log("Next Generation --------------");
+        console.log(child.childNodes);
+
+        if (child.hasChildNodes()) {
+          logChildrenOfNode(child);
+        }
+      }
+    }
+  }
+
+  addValue(value) {}
+
+  // Function der leder efter den givne value i træet, og returnerer den (første) Node der har den
+  findValue(value) {
+    console.log("Find the value: ", value);
+    const treeRoot = this.root;
+    checkNode(treeRoot);
+    // traverseTree(treeRoot);
+
+    // // Traverse the tree to find a node with the value
+    // function traverseTree(node) {
+    //   for (const child of node.childNodes) {
+    //     checkNode(child);
+    //     if (child.hasChildNodes()) {
+    //       traverseTree(child);
+    //     }
+    //   }
+    // }
+
+    // Test if the node has the value
+    function checkNode(node) {
+      if (node.value === value) {
+        return node;
+      }
+    }
+  }
+
+  removeValue(value) {}
+
+  // Udskriver vores note
+  // kigger på vores nodes'children og udskrive dem
+  // og så deres children...
 }
 
 export { Node, Tree };
